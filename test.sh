31a9f01e3479522ed4c0e8e87f270b2efa9fa324
Youjin Jang <jangyoujin2003@gmail.com>
2025-09-24 11:19:26 +0900

    type checking of simplest program success

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 opam switch 5.1.0
 eval $(opam env)
-make
-./spectec-core run-il nanorust_spec/*.spectec -p test.nrst
+make exe
+./spectec-core elab nanorust_spec/*.spectec > output.spectec
+./spectec-core run-il nanorust_spec/*.spectec -p test.nrst -dbg > output.txt
